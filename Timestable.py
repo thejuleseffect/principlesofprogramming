7122adb419cf489a42b6ad2936db2b8c2612cf34
regentoop <20163981+regentoop@users.noreply.github.com>
2025-02-07 11:25:15 +0000

    Create Timestable.py

---
+++ Timestable.py
@@ -0,0 +1,7 @@
+# 1. Give a guess what you think this code is going to do before you run it.
+num = int(input("Enter a number: "))   #2. What do you think this line is going to do?
+
+
+print(f"\nTimes Table for {num}:\n     #3. There is an error on this line can you find it and fix it?     
+for i in range(1, 13):                 #4. Any guess as to what this strange line is supposed to do?
+    print(f"{num} × {i} = num * i}")  #5. There's an error here too. Try to fix it.
